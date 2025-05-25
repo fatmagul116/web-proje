f9f8976ece80fc84375f0b0409709b9fdfac29dd
fatmagul116 <fevkaladebirsahis@gmail.com>
2025-05-25 19:44:12 +0300

    Update login.php

--- login.php
+++ login.php
@@ -112,8 +112,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-    <script>
-        // Client-side JS remains the same as it doesn't handle login logic anymore
-    </script>
+  
 </body>
 </html>
