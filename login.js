257fa8aea7349952b0b6485de4f174f8a3c9edb4
fatmagul116 <fevkaladebirsahis@gmail.com>
2025-05-25 19:43:04 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
